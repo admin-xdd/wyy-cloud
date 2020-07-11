3da37202f650df35a9e9a624035b4b96c7f599f9
1914942840 <58558663+1914942840@users.noreply.github.com>
2020-07-11 20:21:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,6 @@ class Task(object):
             "| 升级提示 |      " + self.tip + "       |\n"
             "------\n"
             "### 任务状态\n" + str(state) + "\n\n"
-            "### 考研倒计时\n距考研还有" + str(date) + "天，主人要加油学习啊\n"
             "### 今日一句\n" + one + "\n\n")
 
     '''
